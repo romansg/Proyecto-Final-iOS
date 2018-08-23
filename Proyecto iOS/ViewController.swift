677df49fd4b7012cbffe0a3a0b43d883e626e89d
romansg <romans@unam.mx>
2018-08-22 20:30:13 -0500

    Initial Commit

---
+++ Proyecto iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Proyecto iOS
+//
+//  Created by Román Sánchez on 22/08/18.
+//  Copyright © 2018 App Gee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
